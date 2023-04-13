6412e958b9bef371f1ce47b9d5ba75e313e7ca25
abusufian12 <abusufian12@gmail.com>
2023-04-14 00:43:20 +0900

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host : 'localhost',
       ref  : 'origin/main',
       repo : 'git@github.com:abusufian12/nextjs-blog.git',
-      path : '/home/sufian',
+      path : '/home/sufian/nextjs-test',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
